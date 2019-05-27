c4651342a7c04076bc59ccb152ba22cc75d6a4a3
javazhuweiqiang <1367724439@qq.com>
2019-05-27 16:15:04 +0800

    project frame

---
+++ application/index/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    public function index()
+    {
+        return "index";
+    }
+    public function hello(){
+        return "hello";
+    }
+}
